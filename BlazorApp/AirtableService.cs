2495d43334caa565304a25b3111d2aad0b488734
dlashchenkov <41324813+dlashchenkov@users.noreply.github.com>
2023-10-03 12:22:23 +0300

    initial

---
+++ BlazorApp/AirtableService.cs
@@ -0,0 +1,70 @@
+﻿using AirtableApiClient;
+
+namespace BlazorApp
+{
+    public interface IAirtableService
+    {
+        Task<List<string>> GetData();
+    }
+
+    public class AirtableService : IAirtableService
+    {
+        readonly string baseId = "appCFFNWJkTCIg8kI";
+        readonly string appKey = "patJU7sCbROmDa8rH.ceddd12a85144dc29d3cf6373a2a7bf4bdd3dfa5a8c0c3c0aade1213e90b3e68";
+        readonly string tableId = "tblrlkMfT33IsAUvG";
+        readonly string fieldName = "Phrase";
+        public async Task<List<string>> GetData()
+        {
+            var result = new List<string>();
+            string offset = null;
+            string errorMessage = null;
+            var records = new List<AirtableRecord>();
+
+            using (AirtableBase airtableBase = new AirtableBase(appKey, baseId))
+            {
+                do
+                {
+                    Task<AirtableListRecordsResponse> task = airtableBase.ListRecords(tableId);
+
+                    AirtableListRecordsResponse response = await task;
+
+                    if (response.Success)
+                    {
+                        records.AddRange(response.Records.ToList());
+                        offset = response.Offset;
+                    }
+                    else if (response.AirtableApiError is AirtableApiException)
+                    {
+                        errorMessage = response.AirtableApiError.ErrorMessage;
+                        if (response.AirtableApiError is AirtableInvalidRequestException)
+                        {
+                            errorMessage += "\nDetailed error message: ";
+                            errorMessage += response.AirtableApiError.DetailedErrorMessage;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        errorMessage = "Unknown error";
+                        break;
+                    }
+                } while (offset != null);
+            }
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                // Error reporting
+                throw new Exception(errorMessage);
+            }
+            else
+            {
+                foreach (var r in records)
+                {
+                    string value = r.GetField(fieldName).ToString();
+                    result.Add(value);
+                }
+            }
+            return result;
+        }
+    }
+}
